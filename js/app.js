dd635f19b9ad46e0b87331981a07bdac4a3e019a
Dylan Zincone <dylan.zincone@gmail.com>
2015-06-03 08:30:07 -0600

    Updated pages to link to subreddits

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-alert("hi");
+
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("get", "http://www.reddit.com/subreddits.json", true);
+xhr.addEventListener("load", function () {
+
+  var response = xhr.response;
+  var data = JSON.parse(response);
+
+  for (var i = 0; i < data.data.children.length; i++) {
+  var p = document.createElement("p");
+  var a = document.createElement("a");
+  a.href = "/index2.html" + "?" + data.data.children[i].data.url;
+  p.appendChild(a);
+  a.innerHTML = data.data.children[i].data.title;
+  document.body.appendChild(p);
+
+
+
+}
+
+});
+xhr.send();
