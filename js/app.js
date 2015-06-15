f25915da8ed29e05b0c607ca03e596cea660cc82
Dylan Zincone <dylan.zincone@gmail.com>
2015-06-14 18:30:27 -0600

    Updated querystring

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@ xhr.addEventListener("load", function () {
   for (var i = 0; i < data.data.children.length; i++) {
   var p = document.createElement("p");
   var a = document.createElement("a");
-  a.href = "/index2.html" + "?" + data.data.children[i].data.url;
+  a.href = "/index2.html" + "?=" + data.data.children[i].data.url;
   p.appendChild(a);
-  a.innerHTML = data.data.children[i].data.title;
+  a.innerHTML = data.data.children[i].data.display_name;
   document.body.appendChild(p);
 
 
--- js/index2.js
+++ js/index2.js
@@ -1,6 +1,16 @@
 var xhr = new XMLHttpRequest();
-var subreddit = document.location.search.replace("?", "")
-xhr.open("get", "http://www.reddit.com/" + subreddit + ".json", true);
+
+var parseQuery = function (string) {
+var o = {};
+string = string.replace("?","");
+var s = string.split("&");
+for(var i = 0; i < s.length; i++) {
+    o[s[i].split("=")[0]] = s[i].split("=")[1]
+  }
+return(o);
+}
+var subreddit = parseQuery(document.location.search);
+xhr.open("get", "http://www.reddit.com/" + subreddit[""] + ".json", true);
 xhr.addEventListener("load", function () {
 
   var response = xhr.response;
@@ -9,7 +19,7 @@ xhr.addEventListener("load", function () {
   for (var i = 0; i < data.data.children.length; i++) {
   var p = document.createElement("p");
   var a = document.createElement("a");
-  a.href = "/index2.html" + "?" + data.data.children[i].data.url;
+  a.href = data.data.children[i].data.url;
   p.appendChild(a);
   a.innerHTML = data.data.children[i].data.title;
   document.body.appendChild(p);
